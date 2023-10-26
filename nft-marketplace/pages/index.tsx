9d955865972ad3ed48f7fa09150f00b422ad251e
Marta Diez Asensio <114190534+MartaDiezAsensio@users.noreply.github.com>
2023-10-26 08:59:45 +0200

    doing

--- nft-marketplace/pages/index.tsx
+++ nft-marketplace/pages/index.tsx
@@ -1,11 +1,19 @@
-import { Container, Heading } from "@chakra-ui/react";
+import { Container, Heading, Flex, Stack, Button } from "@chakra-ui/react";
+import NextLink from 'next/link';
 //import Image from "next/image";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
 	return (
-	<Container>
-		<Heading>Coerumarket</Heading>
+	<Container maxW={"1200px"}>
+		<Flex h={"80vh"} alignItems={"center"} justifyContent={"center"}>
+			<Stack spacing={4} align={"center"}>
+			<Heading>Coerumarket</Heading>
+			<Button
+			as={NextLink} href='/buy'
+			>Shop NFTs</Button>
+			</Stack>
+		</Flex>
 	</Container>
 	);
 };
